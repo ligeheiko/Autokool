48f84559a7835f82e803169e0091d32ee20472f4
Ardo Roosild <ardo.roosild@ttu.ee>
2021-09-13 10:12:50 +0300

    Klassid tehtud, sisu osaliselt puudu

---
+++ Domain/Base.cs
@@ -0,0 +1,7 @@
+﻿
+namespace Domain;
+public class Base
+{
+    public string ID { get; set; }
+
+}
---
+++ Domain/School.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    class School : Base
+    {
+        public List<Administrator> Administrators { get; }
+        public List<Exam> Exams {  get; }
+        public List<Course> Courses {  get; }
+        public List<Student> Students { get; }
+        public void AddNewCourse(Course c, Teacher t, Administrator a) { throw new NotImplementedException(); }
+        public void AddTeacher(Teacher t, Administrator a) { throw new NotImplementedException(); }
+        public void Remove(Teacher t, Administrator a) { throw new NotImplementedException(); }
+        public void AddStudent(Student s, Administrator a) { throw new NotImplementedException(); }
+        public void RemoveStudent(Student s, Administrator a) { throw new NotImplementedException(); }
+        public void UpdateTeacher(Teacher oldT, Teacher newT, Administrator a) { throw new NotImplementedException(); }
+        public void UpdateCourse(Course oldC, Course newC, Administrator a) { throw new NotImplementedException(); }
+
+
+    }
+}
