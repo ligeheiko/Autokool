efd9cfa5904a694e5faafa744ce27f57ab115552
Heiko Lige <helige@ttu.ee>
2021-09-13 21:02:55 +0300

    Skeleti parandus v2.

--- Core/Class1.cs
+++ Core/Class1.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Autokool.Core
+namespace Core
 {
-    class Class1
+    public class Class1
     {
     }
 }
--- Pages/Class1.cs
+++ Pages/Class1.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Autokool.Pages
+namespace Pages
 {
-    class Class1
+    public class Class1
     {
     }
 }
