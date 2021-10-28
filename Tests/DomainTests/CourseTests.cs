dd08fc8313c8d8117ac871c607dec7529e8c8fda
Heiko Lige <helige@ttu.ee>
2021-10-28 14:38:50 +0300

    jatka loeng 109

--- Tests/DomainTests/CourseTests.cs
+++ Tests/DomainTests/CourseTests.cs
@@ -3,6 +3,7 @@ using Autokool.Data.DrivingSchool;
 using Autokool.Domain;
 using Autokool.Domain.Common;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
 
 namespace Autokool.Tests.DomainTests
 {
@@ -25,9 +26,17 @@ namespace Autokool.Tests.DomainTests
         [TestMethod]
         public void CourseTypeIDTest() => isProperty(data.CourseTypeID);
         [TestMethod]
-        public void CourseTypeTest()
+        public async Task CourseTypeTest()
         {
             var repo = new RepoMock<CourseType>();
+            var d = GetRandom.ObjectOf<CourseTypeData>();
+            d.ID = data.CourseTypeID;
+            await repo.Add(new CourseType(d));
+            var p = (obj as Course).CourseType;
+            isNotNull(p);
+            areEqual(d.ID, p.ID);
+            areEqual(d.DrivingCourse, p.DrivingCourse);
+            areEqual(d.TheoryCourse, p.TheoryCourse);
         }
     }
 }
