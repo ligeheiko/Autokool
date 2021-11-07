51b1b880eb54e38aee544ff02c516d77658edc4d
Heiko Lige <helige@ttu.ee>
2021-11-07 17:56:25 +0200

    CourseTypeTest korras

--- Tests/DomainTests/CourseTests.cs
+++ Tests/DomainTests/CourseTests.cs
@@ -2,8 +2,8 @@
 using Autokool.Data.DrivingSchool;
 using Autokool.Domain;
 using Autokool.Domain.Common;
+using Autokool.Domain.Repos;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Threading.Tasks;
 
 namespace Autokool.Tests.DomainTests
 {
@@ -11,6 +11,9 @@ namespace Autokool.Tests.DomainTests
     public class CourseTests : SealedTests<DateEntity<CourseData>>
     {
         private CourseData data;
+        private CourseTypeData courseTypeData;
+
+        private class MockCourseTypeRepo : RepoMock<CourseType>, ICourseTypeRepo { }
         protected override object createObject()
         {
             return new Course(data);
@@ -19,25 +22,46 @@ namespace Autokool.Tests.DomainTests
         public override void TestInitialize()
         {
             data = GetRandom.ObjectOf<CourseData>();
+            createMockCourseTypeRepo();
             base.TestInitialize();
         }
+
+        private void createMockCourseTypeRepo()
+        {
+            var count = GetRandom.UInt8(10, 20);
+            var index = GetRandom.UInt8(0, count);
+            var repo = new MockCourseTypeRepo();
+            for (var i = 0; i < count; i++)
+            {
+                var d = GetRandom.ObjectOf<CourseTypeData>();
+                if (index == i)
+                {
+                    d.ID = data.CourseTypeID;
+                    courseTypeData = d;
+                }
+                repo.Add(new CourseType(courseTypeData)).GetAwaiter();
+            }
+            GetRepo.SetServiceProvider(new ServiceProviderMock(repo));
+        }
+
         [TestMethod]
         public void LocationTest() => isProperty(data.Location);
         [TestMethod]
         public void CourseTypeIDTest() => isProperty(data.CourseTypeID);
         [TestMethod]
-        public async Task CourseTypeTest()
+        public void CourseTypeTest()
         {
-            var repo = new RepoMock<CourseType>();
-            var d = GetRandom.ObjectOf<CourseTypeData>();
-            d.ID = data.CourseTypeID;
-            await repo.Add(new CourseType(d));
-            GetRepo.SetServiceProvider(new ServiceProviderMock(repo));
             var p = (obj as Course).CourseType;
             isNotNull(p);
-            areEqual(d.ID, p.ID);
-            areEqual(d.DrivingCourse, p.DrivingCourse);
-            areEqual(d.TheoryCourse, p.TheoryCourse);
+            areEqual(courseTypeData.ID, p.ID);
+            areEqual(courseTypeData.DrivingCourse, p.DrivingCourse);
+            areEqual(courseTypeData.TheoryCourse, p.TheoryCourse);
+        }
+        [TestMethod]
+        public void CourseTypeIsNullTest()
+        {
+            GetRepo.SetServiceProvider(null);
+            isNull((obj as Course).CourseType);
         }
     }
 }
