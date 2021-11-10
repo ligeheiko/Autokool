ee528163b93cbdffb79216d39349aa2853d56946
Heiko Lige <helige@ttu.ee>
2021-11-10 18:02:02 +0200

    CourseTypes page tootab

---
+++ Pages/Autokool/CourseTypesPage.cs
@@ -0,0 +1,23 @@
+﻿using Autokool.Data.DrivingSchool;
+using Autokool.Domain;
+using Autokool.Domain.Repos;
+using Autokool.Facade.DrivingSchool;
+using Autokool.Pages.Common;
+using System;
+
+namespace Autokool.Pages.Autokool
+{
+    public class CourseTypesPage : ViewPage<CourseTypesPage, ICourseTypeRepo, CourseType, CourseTypeView, CourseTypeData>
+    {
+        public CourseTypesPage(ICourseTypeRepo r) : base(r, "CourseTypes") {}
+        protected override Uri pageUrl() => new Uri("/Administrator/CourseTypes", UriKind.Relative);
+        protected internal override CourseType toObject(CourseTypeView v) => new CourseTypeViewFactory().Create(v);
+        protected internal override CourseTypeView toView(CourseType o) => new CourseTypeViewFactory().Create(o);
+        protected override void createTableColumns()
+        {
+            createColumn(x => Item.ID);
+            createColumn(x => Item.Name);
+        }
+    }
+}
+
