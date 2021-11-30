19777ad9cba1e7d1f2fcc42b088d6ca75c9e2e8d
Heiko Lige <helige@ttu.ee>
2021-11-30 15:23:31 +0200

    DrivingPractice students page + course exam ja driving practice view ValidFrom ja To parandus

--- Facade/DrivingSchool/ViewModels/DrivingPracticeView.cs
+++ Facade/DrivingSchool/ViewModels/DrivingPracticeView.cs
@@ -1,5 +1,7 @@
 ﻿using Autokool.Facade.Common;
+using System;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace Autokool.Facade.DrivingSchool.ViewModels
 {
@@ -7,5 +9,13 @@ namespace Autokool.Facade.DrivingSchool.ViewModels
     {
         [DisplayName("Teacher's Name")]
         public string TeacherID { get; set; }
+
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd HH:mm}")]
+        [Display(Name = "Ends")]
+        public new DateTime ValidTo { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd HH:mm}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Starts")]
+        public new DateTime ValidFrom { get; set; }
     }
 }
---
+++ Pages/Autokool/Students/DrivingPracticeStudentPage.cs
@@ -0,0 +1,35 @@
+﻿using Autokool.Domain.DrivingSchool.Repos;
+using Autokool.Pages.Autokool.Base;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace Autokool.Pages.Autokool.Students
+{
+    public class DrivingPracticeStudentPage : DrivingPracticeBasePage<DrivingPracticeStudentPage>
+    {
+        public DrivingPracticeStudentPage(IDrivingPracticeRepo d, ITeacherRepo t) : base(d, t) { }
+        protected override Uri pageUrl() => new Uri("/Student/DrivingPractices", UriKind.Relative);
+        public override async Task OnGetIndexAsync(string sortOrder,
+           string id, string currentFilter, string searchString, int? pageIndex,
+           string fixedFilter, string fixedValue, bool isRegistered)
+        {
+            getRegistered();
+            SelectedId = id;
+            setIsRegistered(isRegistered);
+            await getList(sortOrder, currentFilter, searchString, pageIndex,
+                fixedFilter, fixedValue).ConfigureAwait(true);
+        }
+
+        public override async Task<IActionResult> OnGetDetailsAsync(string id, string sortOrder, string searchString,
+           int pageIndex,
+           string fixedFilter, string fixedValue, bool register)
+        {
+            await getObject(id, sortOrder, searchString, pageIndex, fixedFilter, fixedValue).ConfigureAwait(true);
+            register = true;
+            setIsRegistered(register);
+            return Page();
+            //TODO registreerimis nupule vajutades saab admin vaadata kes on registreerinud
+        }
+    }
+}
