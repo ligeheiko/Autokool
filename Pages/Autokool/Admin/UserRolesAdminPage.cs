f4ab302e48f1c15934372704e51c27e22102ea23
Heiko Lige <helige@ttu.ee>
2021-12-01 18:04:52 +0200

    User Roles page

---
+++ Pages/Autokool/Admin/UserRolesAdminPage.cs
@@ -0,0 +1,49 @@
+﻿using Autokool.Data.Common;
+using Autokool.Data.DrivingSchool;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autokool.Pages.Autokool.Admin
+{
+    public class UserRolesAdminPage : PageModel
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public List<ApplicationUser> users;
+        public List<UserRolesData> usersData;
+
+        public UserRolesAdminPage(UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> OnGetIndexAsync()
+        {
+            users = await _userManager.Users.ToListAsync();
+            usersData = new List<UserRolesData>();
+            foreach (ApplicationUser user in users)
+            {
+                var thisViewModel = new UserRolesData();
+                thisViewModel.UserId = user.Id;
+                thisViewModel.Email = user.Email;
+                thisViewModel.FirstName = user.FirstName;
+                thisViewModel.LastName = user.LastName;
+                thisViewModel.Roles = await GetUserRoles(user);
+                usersData.Add(thisViewModel);
+            }
+            return Page();
+        }
+        private async Task<List<string>> GetUserRoles(ApplicationUser user)
+        {
+            return new List<string>(await _userManager.GetRolesAsync(user));
+        }
+    }
+}
