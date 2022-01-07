0b40a49ea33c247d27bf802d6c4f32c8002d7fff
Heiko Lige <helige@ttu.ee>
2022-01-07 12:12:35 +0200

    enne .NET 6 minemist

---
+++ Tests/SoftTests/BaseWebPageTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autokool.Tests.SoftTests
+{
+    [TestClass]
+    public class BaseWebPageTests : TestAssertions
+    {
+        [TestMethod]
+        public void SendRequestTest()
+        {
+            dynamic html = validateRequest("/Index");
+            notTested();
+        }
+
+        private dynamic validateRequest(string url)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
